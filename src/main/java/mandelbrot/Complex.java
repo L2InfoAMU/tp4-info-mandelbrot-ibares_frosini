38fff898bb41af285acc34047dc1104feb8ac53b
KIB00631 <kevin.ibares@gmail.com>
2018-11-07 10:38:03 +0100

    Correction des getters des attributs real et imaginary.

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -48,11 +48,13 @@ public class Complex {
     static Complex I = new Complex(0, -1);
 
     double getReal() {
-        return imaginary;
+
+        return this.real;
     }
 
     double getImaginary() {
-        return imaginary;
+
+        return this.imaginary;
     }
 
     /**
