86d9d636f2f55b68923dee6fca4e7a01087959b1
KIB00631 <kevin.ibares@gmail.com>
2018-11-14 10:45:21 +0100

    Correction de la méthode toString

--- src/main/java/mandelbrot/Complex.java
+++ src/main/java/mandelbrot/Complex.java
@@ -227,7 +227,7 @@ public class Complex {
     @Override
     public String toString() {
         return "Complex{" +
-                "real=" + imaginary +
+                "real=" + real +
                 ", imaginary=" + imaginary +
                 '}';
     }
